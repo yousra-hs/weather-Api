6f3d78d64e2e9e3c7e04b355da33dbe9025b690d
yousra-hs <96131919+yousra-hs@users.noreply.github.com>
2022-01-18 12:40:21 +0100

    Update js.js

--- weather Api/js.js
+++ weather Api/js.js
@@ -1,29 +1,21 @@
-   
-var input = document.getElementById("inputCity");
-var button = document.getElementById("button") ;
+var input = document.getElementById('input'); 
+var btn = document.getElementById("button");
 
+btn.addEventListener('click',function verif(){
+    if(input.value ==""){
+        input.nextElementSibling.innerHTML = "Field Required";
+    }
+    var city = document.querySelector('#input').value;
+    apiCall(city);
+});
 
-
-
-
-
-
-
-
-
-
-async function apiCall(city){
-var res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=50a7aa80fa492fa92e874d23ad061374`);
-var data = await res.json();
-
+async function apiCall (weather){
+    var res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${weather}&appid=50a7aa80fa492fa92e874d23ad061374`);
+    var data = await res.json();
 
 document.querySelector("#city").innerHTML = data.name;
 document.querySelector("#temp").innerHTML = data.main.temp;
 document.querySelector("#humidity").innerHTML = data.main.humidity;
 document.querySelector("#wind").innerHTML = data.wind.speed + "km/h";
-}
-document.querySelector('form').addEventListener('submit',function(e){
-    e.preventDefault();
-    var city = document.querySelector('#inputCity').value;
-    apiCall(city);
-})
+ 
+} 
